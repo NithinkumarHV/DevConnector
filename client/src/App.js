9f07723b32e5e6eeaf7d1dd218e95f97e1f0fffa
Nithin_Kumar_HV <nithinkumarhv01@gmail.com>
2019-06-07 23:56:11 +0530

    Create profile started and done

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
+import PrivateRoute from "./components/common/PrivateRoute";
+
 import jwt_decode from "jwt-decode";
 
 import Navbar from "./components/layout/Navbar";
@@ -9,10 +11,13 @@ import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import Dashboard from "./components/dashboard/Dashboard";
+import CreateProfile from "./components/create-profile/CreateProfile";
 
 import "./App.css";
 import setAuthToken from "./utils/setAuthToken";
 import { setCurrentUser, logoutUser } from "./actions/authActions";
+import { clearCurrentProfile } from "./actions/profileActions";
 
 //Check for token
 if (localStorage.jwtToken) {
@@ -27,7 +32,8 @@ if (localStorage.jwtToken) {
   if (decoded.exp < currentTime) {
     //Logout user
     store.dispatch(logoutUser());
-    //TODO: clear current profile
+    //clear current profile
+    store.dispatch(clearCurrentProfile());
     //Redirect to login
     window.location.href("/login");
   }
@@ -44,6 +50,16 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Switch>
+                <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/create-profile"
+                  component={CreateProfile}
+                />
+              </Switch>
             </div>
             <Footer />
           </div>
